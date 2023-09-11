0c2b5b66658988e0364b3679323ad4d6be68a3fc
Boyoung <boyoung.y589@gmail.com>
2023-09-11 19:57:28 +0900

    changed: organize folders

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Boyoung";
+const age = 24;
+const gender = "female";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,8 @@
+const sayHi = (name: string, age: number, gender: string) => { //
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+}
+
+sayHi("Boyoung", 30, "female");
+
+
+export {};
